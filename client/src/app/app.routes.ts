fe9281be833e4dee979f342fadb26eef4a4e973a
phoenix-of-dawn <eliobenoit@live.com>
2025-03-12 15:35:42 -0400

    Add search and game to app component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SearchPage } from './pages/search/search-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent,
+    },
+    {
+        path: 'search',
+        component: SearchPage,
+    },
+];
