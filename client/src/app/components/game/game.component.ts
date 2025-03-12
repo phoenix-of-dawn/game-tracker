744dc47ab31f832fd5b60b663bd0f3d4880cddb8
phoenix-of-dawn <eliobenoit@live.com>
2025-03-12 18:45:35 -0400

    Incorporated Material Design into game components as Cards

--- client/src/app/components/game/game.component.ts
+++ client/src/app/components/game/game.component.ts
@@ -1,11 +1,13 @@
 import { Component, computed, input } from '@angular/core';
 import { GameHandler } from '../../services/game-fetch.service';
 import { Game } from '../../interfaces/game';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
     selector: 'game',
     templateUrl: './game.component.html',
     styleUrl: './game.component.scss',
+    imports: [MatCardModule],
 })
 export class GameComponent {
     gameInput = input<Game>();
