a731276cfb42d5b22209c55747369d1b9697e084
phoenix-of-dawn <eliobenoit@live.com>
2025-03-11 21:01:09 -0400

    Add game handlers

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/phoenix-of-dawn/game-tracker/server/internal/handlers"
 	"github.com/phoenix-of-dawn/game-tracker/server/internal/igdb"
 )
 
 func main() {
 	igdb.Setup()
+
+	router := gin.Default()
+	handlers.Setup(router)
+	log.Println("Starting...")
+	router.Run("localhost:8080")
 }
