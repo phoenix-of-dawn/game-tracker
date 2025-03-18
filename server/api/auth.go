a0133a2227e17a3fd0467b3b811db4aec246783f
phoenix-of-dawn <eliobenoit@live.com>
2025-03-18 10:19:39 -0400

    Make bare user struct

---
+++ server/api/auth.go
@@ -0,0 +1,10 @@
+package api
+
+import "github.com/bwmarrin/snowflake"
+
+type User struct {
+	Id snowflake.ID `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email string `json:"email"`
+}
