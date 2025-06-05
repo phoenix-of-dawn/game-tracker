06c2ee3eacfc378086724d6e8ffd33cffbb12294
phoenix-of-dawn <eliobenoit@live.com>
2025-06-04 20:39:11 -0400

    Create Register component

---
+++ client/src/app/components/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'register-component',
+    templateUrl: 'register.component.html',
+    styleUrl: 'register.component.scss',
+    imports: [],
+})
+export class RegisterComponent {
+    constructor() {}
+}
