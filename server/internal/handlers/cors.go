b3d1f0239beeac9685247a0c390f51297a396a89
phoenix-of-dawn <eliobenoit@live.com>
2025-06-12 19:10:54 -0400

    Fixed CORS

--- server/internal/handlers/cors.go
+++ server/internal/handlers/cors.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +12,11 @@ func CORSMiddleware() gin.HandlerFunc {
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(http.StatusNoContent)
+			return
+		}
 
 		ctx.Next()
 	}
